fa5094937de15c04c89ab5a610ca6f81ded21b28
Scott Donnan <scott.donnan@me.com>
2022-01-03 16:13:20 -0600

    added word and favorite routes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,4 +9,13 @@ class FavoritesController < ApplicationController
         favorite = Favorite.find_by(id: params[:id])
         render json: favorite
     end
+
+    def create
+        favorite = Favorite.create(favorites_params)
+        render json: favorite
+    end
+
+    def favorites_params
+        params.permit(:user_id, :word_id)
+    end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,11 @@
 class WordsController < ApplicationController
+
+    def create
+        word = Word.create(word_params)
+        render json: word
+    end
+
+    def word_params
+        params.permit(:name)
+    end
 end
