cabdb6ecf4230389a8286f096e5dc8a949b384df
Scott Donnan <scott.donnan@me.com>
2022-01-06 09:00:53 -0600

    new commits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionController < ApplicationController
+class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:username])
     if user&.authenticate(params[:password])
