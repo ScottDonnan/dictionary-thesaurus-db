4e012eb41b15c1acd2b13f1b7dd55f2df17f288c
Scott Donnan <scott.donnan@me.com>
2022-01-19 14:18:37 -0600

    changed origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://dictionary-thesaurus.herokuapp.com/:80'
+    origins '*'
 
     resource '*',
       headers: :any,
