b53e5df9323bf47ec038d588527e6722275d27eb
Scott Donnan <scott.donnan@me.com>
2022-01-19 14:26:16 -0600

    commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://dictionary-thesaurus.herokuapp.com/'
+    origins 'https://dictionary-thesaurus.herokuapp.com/:80'
 
     resource '*',
       headers: :any,
